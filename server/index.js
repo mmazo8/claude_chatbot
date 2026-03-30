357b694aeb94f5ffebe57ca8a10a6505eee45a90
Michelle Mazo <mmazo.ucsd@gmail.com>
2026-03-30 12:19:41 -0700

    changed compactiion to 600K

--- server/index.js
+++ server/index.js
@@ -493,7 +493,7 @@ function buildAnthropicRequestBody({
     edits: [
       {
         type: "compact_20260112",
-        "trigger": {"type": "input_tokens", "value": 300000},
+        "trigger": {"type": "input_tokens", "value": 600000},
       },
     ],
   };
